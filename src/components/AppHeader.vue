ea4ff15ef9bc67f0aaf1d6b71402c624a65ca61e
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-05 20:49:00 +0200

    Create header components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'Home',
+                    route_name: 'homepage'
+                },
+                {
+                    label: 'Servizi',
+                    route_name: 'services'
+                },
+                {
+                    label: 'Progetti',
+                    route_name: 'projects'
+                },
+                {
+                    label: 'Chi siamo',
+                    route_name: 'about_us'
+                },
+                {
+                    label: 'Contattaci',
+                    route_name: 'ask_us'
+                },
+                {
+                    label: 'Blog',
+                    route_name: 'blog'
+                }
+            ]
+
+
+        }
+    },
+}
+</script>
+
+<template>
+    <header class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <span v-for="(item, index) in menu" :key="index">
+                        {{ item.label }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss"></style>
