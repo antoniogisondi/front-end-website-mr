d09d5b9a58929b5a46d52f8cc328bb90da7e4849
fbisceglia02 <ferruccio3214@gmail.com>
2023-09-22 18:15:00 +0200

    feat: modify url

--- src/data/store.js
+++ src/data/store.js
@@ -2,6 +2,6 @@ import { reactive } from "vue";
 
 export const store = reactive({
     baseUrl: 'http://127.0.0.1:8000',
-    hostUrl: 'http://20.253.35.9:8080/',
+    hostUrl: 'http://20.253.35.9:8080',
     loading: true
 })
