8225faf72c3676198f07cc553c13456fb789b220
antoniogisondi <anto.gisondi@tiscali.it>
2023-10-09 20:02:13 +0200

    [FEAT] I'm Back

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -37,17 +37,18 @@ export default {
                     <!-- Immagine -->
                     <img v-if="type.cover_image" :src="`${this.store.baseUrl}/storage/${type.cover_image}`" :alt="type.slug"
                         class="img-fluid card-img-top">
-                    <img v-else src="../assets/logo-mr.png" alt="logo" class="img-fluid card-img-top">
+                    <img v-else src="../assets/placeholder-image.jpg" alt="logo" class="img-fluid card-img-top">
 
                     <!-- Testo della card -->
                     <div class="card-body text-center">
-                        <router-link class="btn btn-sm btn-outline-info"
-                            :to="{ name: 'works', params: { slug: type.slug } }">SCOPRI DI PIU'</router-link>
+                        <h3>{{ type.nome_tipologia }}</h3>
                     </div>
 
                     <!-- Testo sovrapposto -->
                     <div class="text">
-                        <h3>{{ type.nome_tipologia }}</h3>
+                        <router-link :to="{ name: 'works', params: { slug: type.slug } }">
+                            <button class="bn632-hover bn26">SCOPRI DI PIU'</button>
+                        </router-link>
                     </div>
                 </div>
             </div>
@@ -64,5 +65,45 @@ export default {
     .card-size {
         width: 20rem;
     }
+}
+
+// BUTTON EFFECTS
+.bn632-hover {
+    width: 160px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    margin: 20px;
+    height: 55px;
+    text-align: center;
+    border: none;
+    background-size: 300% 100%;
+    border-radius: 50px;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.bn632-hover:hover {
+    background-position: 100% 0;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.bn632-hover:focus {
+    outline: none;
+}
+
+.bn632-hover.bn26 {
+    background-image: linear-gradient(to right,
+            #25aae1,
+            #4481eb,
+            #04befe,
+            #3f86ed);
+    box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
 }
 </style>
