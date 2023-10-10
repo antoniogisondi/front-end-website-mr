b39c1916ab211006a92e7fbf6a072350c25d9541
antoniogisondi <anto.gisondi@tiscali.it>
2023-10-10 21:15:41 +0200

    [FEAT] Add correlation images from back-end api calls works

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -48,56 +48,112 @@ export default {
 }
 </script>
 <template>
-    <div class="container container-custom-contact-us">
-        <div class="row">
+    <div class="container card w-25 mt-5">
+        <div class="row mt-3 mb-5">
             <div class="col">
-                <h1 class="heading">Contattaci</h1>
+                <h1 class="text-center">Contattaci</h1>
             </div>
         </div>
-        <div class="row">
+        <form @submit.prevent="sendMail()" class="row d-flex justify-content-center">
+            <div class="col-10 d-flex flex-column p-2">
+                <label for="name" class="custom-label">Nome e Cognome *</label>
+                <input type="text" name="name" id="name" v-model="name" class="custom-input" placeholder="Nome e Cognome"
+                    :class="errors.name ? 'is-invalid' : ''" required>
+                <div v-for="(error, index) in errors.name" :key="index" class="text-danger">
+                    <p class="custom-error">
+                        {{ error }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-10 d-flex flex-column p-2">
+                <label for="email" class="custom-label">Email *</label>
+                <input type="text" name="email" id="email" v-model="email" class="custom-input"
+                    placeholder="email@example.com" :class="errors.email ? 'is-invalid' : ''" required>
+                <div v-for="(error, index) in errors.email" :key="index" class="text-danger">
+                    <p class="custom-error">
+                        {{ error }}
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-10 d-flex flex-column p-3">
+                <label for="message" class="custom-label">Messaggio *</label>
+                <textarea name="message" id="message" v-model="message" class="custom-textarea" placeholder="Messaggio"
+                    :class="errors.content ? 'is-invalid' : ''"></textarea>
+                <div v-for="(error, index) in errors.description" :key="index" class="text-danger">
+                    <p class="custom-error">
+                        {{ error }}
+                    </p>
+                </div>
+            </div>
+
             <div class="col-12">
-                <form @submit.prevent="sendMail()" class="row">
-                    <div class="col-12">
-                        <label for="name" class="custom-label">Nome e Cognome</label>
-                        <input type="text" name="name" id="name" v-model="name" class="custom-input"
-                            placeholder="Nome e Cognome" :class="errors.name ? 'is-invalid' : ''">
-                        <div v-for="(error, index) in errors.name" :key="index" class="text-danger">
-                            <p class="custom-error">
-                                {{ error }}
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <label for="email" class="custom-label">Email</label>
-                        <input type="text" name="email" id="email" v-model="email" class="custom-input"
-                            placeholder="email@example.com" :class="errors.email ? 'is-invalid' : ''">
-                        <div v-for="(error, index) in errors.email" :key="index" class="text-danger">
-                            <p class="custom-error">
-                                {{ error }}
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <label for="message" class="custom-label">Messaggio</label>
-                        <textarea name="message" id="message" v-model="message" class="custom-textarea"
-                            placeholder="Messaggio" :class="errors.content ? 'is-invalid' : ''"></textarea>
-                        <div v-for="(error, index) in errors.description" :key="index" class="text-danger">
-                            <p class="custom-error">
-                                {{ error }}
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <button v-if="loading" type="submit" class="custom-button" :disabled="loading">Invio in
-                            corso...</button>
-                        <button v-else class="custom-button">Invio</button>
-                    </div>
-                </form>
+                <button v-if="loading" class="bn632-hover bn22">Invio in
+                    corso...</button>
+                <button v-else class="bn632-hover bn22">Invio</button>
             </div>
-        </div>
+        </form>
     </div>
 </template>
-<style lang="scss"></style>
+
+<style lang="scss">
+@use '../styles/generals.scss' as *;
+
+.custom-input {
+    padding: 10px;
+    border-radius: 10px;
+    border: 0;
+    box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+}
+
+.custom-textarea {
+    padding: 10px;
+    border-radius: 10px;
+    border: 0;
+    box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+}
+
+
+// CONTACT US BUTTONS EFFECTS
+.bn632-hover {
+    width: 160px;
+    font-size: 16px;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    margin: 20px;
+    height: 55px;
+    text-align: center;
+    border: none;
+    background-size: 300% 100%;
+    border-radius: 50px;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.bn632-hover:hover {
+    background-position: 100% 0;
+    moz-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    -webkit-transition: all .4s ease-in-out;
+    transition: all .4s ease-in-out;
+}
+
+.bn632-hover:focus {
+    outline: none;
+}
+
+.bn632-hover.bn22 {
+    background-image: linear-gradient(to right,
+            #0ba360,
+            #3cba92,
+            #30dd8a,
+            #2bb673);
+    box-shadow: 0 4px 15px 0 rgba(23, 168, 108, 0.75);
+}
+
+// END
+</style>
--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -23,17 +23,31 @@ export default {
                     this.$router.push({ name: 'thank-you' });
                 }
             })
-        }
+        },
     },
 }
 </script>
 <template>
-    <div>
-        <div class="custom-row">
-            <div class="custom-col">
-                <div class="custom-project" v-for="   work    in    works   " :key="work.id">
+    <div class="container">
+        <div class="row">
+            <div class="col-5" v-for="   work    in    works   " :key="work.id">
+                <div class="card">
                     <h2 class="custom-project-title">{{ work.titolo }}</h2>
                     <p class="custom-project-description">{{ work.descrizione }}</p>
+                    <span>€{{ work.costo }}</span>
+                    <span>{{ work.data_inizio }}</span>
+                    <span>{{ work.data_fine }}</span>
+                    <span>{{ work.cliente }}</span>
+                    <span>{{ work.indirizzo_lavoro }}</span>
+                    <span>{{ work.responsabile_lavoro }}</span>
+                    <span>{{ work.materiali }}</span>
+                    <div v-for="item in work.images" :key="item.id">
+                        <img :src="`${this.store.baseUrl}/storage/${item.image}`" alt="work-image" class="img-fluid w-25">
+                        <img :src="`${this.store.baseUrl}/storage/${item.image_2}`" alt="work-image" class="img-fluid w-25">
+                        <img :src="`${this.store.baseUrl}/storage/${item.image_3}`" alt="work-image" class="img-fluid w-25">
+                        <img :src="`${this.store.baseUrl}/storage/${item.image_4}`" alt="work-image" class="img-fluid w-25">
+                        <img :src="`${this.store.baseUrl}/storage/${item.image_5}`" alt="work-image" class="img-fluid w-25">
+                    </div>
                 </div>
             </div>
         </div>
